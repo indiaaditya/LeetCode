db9c78ecbf8c454320d0994c635aae645bda9758
indiaaditya <indiaaditya@gmail.com>
2020-08-16 01:49:24 +0530

    add2Numbers edge cases handling2

--- Add2Numbers/a2n.cpp
+++ Add2Numbers/a2n.cpp
@@ -3,79 +3,93 @@ using namespace std;
 
 /**
  * Definition for singly-linked list.*/
-  struct ListNode {
-      int val;
-      ListNode *next;
-      ListNode() : val(0), next(nullptr) {}
-      ListNode(int x) : val(x), next(nullptr) {}
-      ListNode(int x, ListNode *next) : val(x), next(next) {}
-  };
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
 
-class Solution {
+class Solution
+{
 public:
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-     int carry = 0;
-     ListNode* retNodeFirst;
-     ListNode* nextNode;  
-     int cntr = 0; 
-     int i;
-     while((l1 != NULL) && (l2 != NULL)){         
-         i = (l1->val + l2->val);
-         if(carry == 1){
-             i++;
-             carry = 0;
-         }
-         if(i > 9){
-             carry = 1;
-             i -= 10;
-         }
+    ListNode *addTwoNumbers(ListNode *l1, ListNode *l2)
+    {
+        int carry = 0;
+        ListNode *retNodeFirst;
+        ListNode *nextNode;
+        int cntr = 0;
+        int i;
+        while ((l1 != NULL) && (l2 != NULL))
+        {
+            i = (l1->val + l2->val);
+            if (carry == 1)
+            {
+                i++;
+                carry = 0;
+            }
+            if (i > 9)
+            {
+                carry = 1;
+                i -= 10;
+            }
 
-        if(cntr == 0)
-            retNodeFirst = createHeadNode(i);   
-        else{
-            if(cntr == 1)
-                nextNode = createNextNode(i,retNodeFirst);
+            if (cntr == 0)
+                retNodeFirst = createHeadNode(i);
             else
-                nextNode = createNextNode(i,nextNode);
+            {
+                if (cntr == 1)
+                    nextNode = createNextNode(i, retNodeFirst);
+                else
+                    nextNode = createNextNode(i, nextNode);
+            }
+            l1 = l1->next;
+            l2 = l2->next;
+            cntr++;
         }
-         l1 = l1->next;
-         l2 = l2->next;
-         cntr++;
-     }
-     while(l1 != NULL){
-         i = l1->val;
-         if(carry == 1){
-             i++;
-             carry = 0;
-         }
-         if(i > 9){
-             carry = 1;
-             i -= 10;
-         }
+        while (l1 != NULL)
+        {
+            i = l1->val;
+            if (carry == 1)
+            {
+                i++;
+                carry = 0;
+            }
+            if (i > 9)
+            {
+                carry = 1;
+                i -= 10;
+            }
 
-         nextNode = createNextNode(l1->val,nextNode);
-         l1 = l1->next;
-         cntr++;
-     }
-     while(l2 != NULL){
-        if(carry == 1){
-             i++;
-             carry = 0;
-         }
-         if(i > 9){
-             carry = 1;
-             i -= 10;
-         }
-         nextNode = createNextNode(i,nextNode);         
-         l2 = l2->next;
-         cntr++;
-     }
-     if(carry == 1)
-        nextNode = createNextNode(1,nextNode);
-     return retNodeFirst;      
+            nextNode = createNextNode(l1->val, nextNode);
+            l1 = l1->next;
+            cntr++;
+        }
+        while (l2 != NULL)
+        {
+            if (carry == 1)
+            {
+                i++;
+                carry = 0;
+            }
+            if (i > 9)
+            {
+                carry = 1;
+                i -= 10;
+            }
+            nextNode = createNextNode(i, nextNode);
+            l2 = l2->next;
+            cntr++;
+        }
+        if (carry == 1)
+            nextNode = createNextNode(1, nextNode);
+        return retNodeFirst;
     }
 
-    ListNode* createHeadNode(int rData){
+    ListNode *createHeadNode(int rData)
+    {
         ListNode *nodeHead;
         nodeHead = new ListNode();
         nodeHead->val = rData;
@@ -83,7 +97,8 @@ public:
         return nodeHead;
     }
 
-    ListNode* createNextNode(int rData, ListNode* rPrevNode){
+    ListNode *createNextNode(int rData, ListNode *rPrevNode)
+    {
         ListNode *nodeNext;
         nodeNext = new ListNode();
         nodeNext->val = rData;
@@ -102,38 +117,40 @@ public:
         }
     }
 
-    ListNode* prefillNode(int* data, int cntr){
-        ListNode *nodeFirst;    
+    ListNode *prefillNode(int *data, int cntr)
+    {
+        ListNode *nodeFirst;
         nodeFirst = createHeadNode(*data);
         cntr--;
         data++;
         ListNode *nodeNext;
-        nodeNext = createNextNode(*data,nodeFirst);
+        nodeNext = createNextNode(*data, nodeFirst);
         cntr--;
         data++;
-        for(int i = 0; i < cntr; i++){
-            nodeNext = createNextNode(*data,nodeNext);
+        for (int i = 0; i < cntr; i++)
+        {
+            nodeNext = createNextNode(*data, nodeNext);
             data++;
         }
         return nodeFirst;
-    } 
+    }
 };
 
-
-int main(){
+int main()
+{
     //declaration of nodes
     ListNode *l1;
     ListNode *l2;
     ListNode *l3;
-    int firstArr[4] = {7,8,9,1};
-    int secondArr[4] = {4,6,7,3};
+    int firstArr[] = {7, 8, 9, 5};
+    int secondArr[] = {4, 6, 7, 9, 1, 1};
 
     Solution s1;
-    l1 = s1.prefillNode(firstArr,4);
-    l2 = s1.prefillNode(secondArr,4);
+    l1 = s1.prefillNode(firstArr, 4);
+    l2 = s1.prefillNode(secondArr, 4);
     s1.printList(l1);
     s1.printList(l2);
-    l3 = s1.addTwoNumbers(l1,l2);
+    l3 = s1.addTwoNumbers(l1, l2);
     s1.printList(l3);
-    cout<< "\nComplete\n"; 
+    cout << "\nComplete\n";
 }
